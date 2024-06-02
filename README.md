2d1c1e2f6b8d76217309691ebbc72557b3a66042
mirandarb <baldridgemiranda@yahoo.com>
2024-06-02 00:06:46 -0400

    added prompts to user input

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Employee-Payroll-Tracker
+# 03 JavaScript: Employee Payroll Tracker
+
+## Your Task
+
+In this weeks challenge I was given starter code to prompt a user to enter employee salary data. 
+
+# User Story
+
+AS A payroll manager
+
+I WANT AN employee payroll tracker
+
+SO THAT I can see my employees' payroll data and properly budget for the company
+
+## Acceptance Criteria
+
+GIVEN an employee payroll tracker
+
+WHEN I click the "Add employee" button
+
+THEN I am presented with a series of prompts asking for first name, last name, and salary
+
+WHEN I finish adding an employee
+
+THEN I am prompted to continue or cancel
+
+WHEN I choose to continue
+
+THEN I am prompted to add a new employee
+
+WHEN I choose to cancel
+
+THEN my employee data is displayed on the page sorted alphabetically by last name, and the console shows computed and aggregated data
+
+
+#
+
+code source: https://git.bootcampcontent.com/boot-camp-consortium-east-coast/FSF-PT-EAST-MAY-050624-A
+
+I also used the AI tool on the bootcamp spot.
---
+++ script.js
@@ -0,0 +1,129 @@
+// Get a reference to the #add-employees-btn element
+const addEmployeesBtn = document.querySelector('#add-employees-btn');
+
+// Collect employee data
+const collectEmployees = function() {
+  // TODO: Get user input to create and return an array of employee objects
+  const employeesArray = [];
+  
+  let addingEmployees = true;
+  while (addingEmployees) {
+    const firstName = prompt("Emter employee's first name:");
+    const lastName = prompt("Enter employee's last name:");
+    const salaryInput = prompt("Enter employee's salary:");
+    const salary = parseFloat(salaryInput) || 0; 
+
+    const employee = {
+      firstName: firstName,
+      lastName: lastName,
+      salary: salary,
+    };
+
+    employeesArray.push(employee);
+
+    const continueInput = prompt("Do you want to add another employee?");
+    addingEmployees = continueInput === "yes";
+  }
+  return employeesArray;
+};
+
+const employees = collectEmployees();
+console.log(employees);
+
+
+// Display the average salary
+const displayAverageSalary = function(employeesArray) {
+  // TODO: Calculate and display the average salary
+  let totalSalary = 0;
+  
+  employeesArray.forEach(employee => {
+    totalSalary += employee.salary;
+  });
+
+  const averageSalary = totalSalary / employeesArray.length;
+
+  console.log(`Average Salary: $${averageSalary.toFixed(2)} for ${employeesArray.length} employees.`);
+
+};
+
+displayAverageSalary(employees);
+
+
+// Select a random employee
+const getRandomEmployee = function(employeesArray) {
+  // TODO: Select and display a random employee
+  const randomIndex = Math.floor(Math.random() * employeesArray.length);
+
+  const randomEmployee = employeesArray[randomIndex];
+
+  console.log(`Congratulation ${randomEmployee.firstName} ${randomEmployee.lastName}! The winner of our random drawing!!`);
+}
+
+getRandomEmployee(employees);
+
+
+/*
+  ====================
+  STARTER CODE
+  Do not modify any of the code below this line:
+*/
+
+// Display employee data in an HTML table
+const displayEmployees = function(employeesArray) {
+  // Get the employee table
+  const employeeTable = document.querySelector('#employee-table');
+
+  // Clear the employee table
+  employeeTable.innerHTML = '';
+
+  // Loop through the employee data and create a row for each employee
+  for (let i = 0; i < employeesArray.length; i++) {
+    const currentEmployee = employeesArray[i];
+
+    const newTableRow = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = currentEmployee.firstName;
+    newTableRow.append(firstNameCell);
+
+    const lastNameCell = document.createElement("td");
+    lastNameCell.textContent = currentEmployee.lastName;
+    newTableRow.append(lastNameCell);
+
+    const salaryCell = document.createElement("td");
+    // Format the salary as currency
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
+      style:"currency",
+      currency:"USD"
+    });
+
+    newTableRow.append(salaryCell);
+
+    employeeTable.append(newTableRow);
+  }
+}
+
+const trackEmployeeData = function() {
+  const employees = collectEmployees();
+
+  console.table(employees);
+
+  displayAverageSalary(employees);
+
+  console.log('==============================');
+
+  getRandomEmployee(employees);
+
+  employees.sort(function(a,b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  displayEmployees(employees);
+}
+
+// Add event listener to 'Add Employees' button
+addEmployeesBtn.addEventListener('click', trackEmployeeData);
